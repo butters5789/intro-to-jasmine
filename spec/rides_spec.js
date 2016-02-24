8b3429d516184dedde248c2815aae250986109c1
Brad Butterfield <butters5789@gmail.com>
2016-02-24 14:25:57 -0700

    rides test 2: determines when a child is tall enough

--- spec/rides_spec.js
+++ spec/rides_spec.js
@@ -2,7 +2,7 @@ var rides = require('../lib/rides');
 
 describe('Rides', function() {
 
-  it('determines when a child is too short', function() { //should result in true when they are too short
+  it('determines when a child is too short', function() { // true = they can ride && false = they can not ride
 
     var child1 = {
       height: 3,
@@ -26,38 +26,42 @@ describe('Rides', function() {
       maxHeight: 7
     };
 
-    expect(rides.isTallEnough(child1, ride1)).toEqual(false);
-    expect(rides.isTallEnough(child1, ride2)).toEqual(true);
-    expect(rides.isTallEnough(child2, ride1)).toEqual(false);
-    expect(rides.isTallEnough(child2, ride2)).toEqual(false);
+    expect(rides.isTallEnough(child1, ride1)).toEqual(true);
+    expect(rides.isTallEnough(child1, ride2)).toEqual(false);
+    expect(rides.isTallEnough(child2, ride1)).toEqual(true);
+    expect(rides.isTallEnough(child2, ride2)).toEqual(true);
 
   });
 
-  // it('determines when a child is tall enough', function() {
-  //
-  //   var child1 = {
-  //     height: 3,
-  //     name: "Julian",
-  //     age: 3
-  //   };
-  //
-  //   var child2 = {
-  //     height: 4,
-  //     name: "Cero",
-  //     age: 4
-  //   };
-  //
-  //   var ride1 = {
-  //     minHeight: 3,
-  //     maxHeight: 7
-  //   };
-  //
-  //   var ride2 = {
-  //     minHeight: 4,
-  //     maxHeight: 7
-  //   };
-  //
-  //   expect(rides.isTallEnough(child1, ride1)).toEqual(true);
-  // });
+  it('determines when a child is tall enough', function() { // true = they can ride && false = they can not ride
+
+    var child1 = {
+      height: 3,
+      name: "Julian",
+      age: 3
+    };
+
+    var child2 = {
+      height: 4,
+      name: "Cero",
+      age: 4
+    };
+
+    var ride1 = {
+      minHeight: 3,
+      maxHeight: 7
+    };
+
+    var ride2 = {
+      minHeight: 4,
+      maxHeight: 7
+    };
+
+    expect(rides.isTallEnough(child1, ride1)).toEqual(true);
+    expect(rides.isTallEnough(child1, ride2)).toEqual(false);
+    expect(rides.isTallEnough(child2, ride1)).toEqual(true);
+    expect(rides.isTallEnough(child2, ride2)).toEqual(true);
+
+  });
 
 });
