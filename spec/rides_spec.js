ab1a72dc59226593235781a1f0fab4489bd44199
Brad Butterfield <butters5789@gmail.com>
2016-02-24 14:10:35 -0700

    rides test 1: determines when a child is too short

---
+++ spec/rides_spec.js
@@ -0,0 +1,63 @@
+var rides = require('../lib/rides');
+
+describe('Rides', function() {
+
+  it('determines when a child is too short', function() { //should result in true when they are too short
+
+    var child1 = {
+      height: 3,
+      name: "Julian",
+      age: 3
+    };
+
+    var child2 = {
+      height: 4,
+      name: "Cero",
+      age: 4
+    };
+
+    var ride1 = {
+      minHeight: 3,
+      maxHeight: 7
+    };
+
+    var ride2 = {
+      minHeight: 4,
+      maxHeight: 7
+    };
+
+    expect(rides.isTallEnough(child1, ride1)).toEqual(false);
+    expect(rides.isTallEnough(child1, ride2)).toEqual(true);
+    expect(rides.isTallEnough(child2, ride1)).toEqual(false);
+    expect(rides.isTallEnough(child2, ride2)).toEqual(false);
+
+  });
+
+  // it('determines when a child is tall enough', function() {
+  //
+  //   var child1 = {
+  //     height: 3,
+  //     name: "Julian",
+  //     age: 3
+  //   };
+  //
+  //   var child2 = {
+  //     height: 4,
+  //     name: "Cero",
+  //     age: 4
+  //   };
+  //
+  //   var ride1 = {
+  //     minHeight: 3,
+  //     maxHeight: 7
+  //   };
+  //
+  //   var ride2 = {
+  //     minHeight: 4,
+  //     maxHeight: 7
+  //   };
+  //
+  //   expect(rides.isTallEnough(child1, ride1)).toEqual(true);
+  // });
+
+});
